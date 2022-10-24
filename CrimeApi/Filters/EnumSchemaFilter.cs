435a5ff2c3255c1c6540dc884c9087be9496db1b
Hubert Zycinski <hzycinski@gmail.com>
2022-10-24 14:15:54 +0200

    Refactor to remove null warnings

--- CrimeApi/Filters/EnumSchemaFilter.cs
+++ CrimeApi/Filters/EnumSchemaFilter.cs
@@ -14,8 +14,8 @@ public class EnumSchemaFilter : ISchemaFilter
             model.Enum.Clear();
             foreach (string enumName in Enum.GetNames(context.Type))
             {
-                System.Reflection.MemberInfo memberInfo = context.Type.GetMember(enumName).FirstOrDefault(m => m.DeclaringType == context.Type);
-                EnumMemberAttribute enumMemberAttribute = memberInfo == null
+                var memberInfo = context.Type.GetMember(enumName).FirstOrDefault(m => m.DeclaringType == context.Type);
+                var enumMemberAttribute = memberInfo == null
                  ? null
                  : memberInfo.GetCustomAttributes(typeof(EnumMemberAttribute), false).OfType<EnumMemberAttribute>().FirstOrDefault();
                 string label = enumMemberAttribute == null || string.IsNullOrWhiteSpace(enumMemberAttribute.Value)
